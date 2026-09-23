665c537cd71513bf9477e77e347bc4cd67b36df9
minseongkim275-dotcom <minseongkim275@gmail.com>
2026-09-23 10:50:13 +0900

    04_double_free closes #126

--- debugging_lab_docker/debugging_lab_docker/challenges/04_double_free/bug.c
+++ debugging_lab_docker/debugging_lab_docker/challenges/04_double_free/bug.c
@@ -101,12 +101,11 @@ static void directory_dump(Directory *d) {
 
 static void directory_free(Directory *d) {
     for (int i = 0; i < d->count; i++) {
-        free(d->by_id[i]->name);
         free(d->by_id[i]);                 
     }
-    for (int i = 0; i < d->count; i++) {
-        free(d->by_name[i]);               
-    }
+    // for (int i = 0; i < d->count; i++) {
+    //     free(d->by_name[i]);               
+    // }
     d->count = 0;
 }
 
